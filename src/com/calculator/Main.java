5be67dd8a07873d1387467c51d56ff930fc1bfb4
Jimmy <jimmy@dlit.co.kr>
2021-08-24 22:53:25 +0900

    Initial commit

---
+++ src/com/calculator/Main.java
@@ -0,0 +1,27 @@
+package com.calculator;
+
+import java.util.Scanner;
+
+public class Main {
+
+    private static int num1, num2;
+    private static String operation;
+    private static double result;
+
+    public static void main(String[] args) throws Exception {
+	// write your code here
+        Scanner console = new Scanner(System.in);
+        System.out.println("Please, enter below an operation for calculation.");
+        operation = console.nextLine();
+        try {
+            Operation op = new Operation(operation);
+            System.out.println(operation + " = " + (op.getResult()));
+        }
+        catch (Exception e){
+            System.out.println("Error: " + e);
+        }
+
+
+
+    }
+}
---
+++ src/com/calculator/Operation.java
@@ -0,0 +1,157 @@
+package com.calculator;
+
+public class Operation {
+    private double result;
+    private double num1, num2;
+
+    private final int MUL = 1, DIV = 2, ADD = 3, SUB = 4;
+    private final int[] arabicNumerals = {1000, 500, 100, 50, 10, 5, 1};
+    private final String[] romanNumerals = {"M", "D", "C", "L", "X", "V", "I"};
+    private Integer operation;
+
+    public Operation() {
+    }
+
+    public Operation(String operation) throws Exception {
+        this.operation = getOperation(operation);
+        switch (this.operation) {
+            case MUL:
+                result = Multiply(num1, num2);
+                break;
+            case DIV:
+                result = Divide(num1, num2);
+                break;
+            case ADD:
+                result = Addition(num1, num2);
+                break;
+            case SUB:
+                result = Subtract(num1, num2);
+                break;
+            default:
+                throw new Exception("Wrong operation was given.");
+        }
+
+
+    }
+
+    private int getOperation(String operation) {
+        if (operation.isEmpty())
+            return -1;
+
+        String[] values = operation.strip().split(" ");
+        num1 = getNum(values[0]);
+        num2 = getNum(values[2]);
+
+        switch (values[1]) {
+            case "*":
+                this.operation = MUL;
+                break;
+            case "/":
+                this.operation = DIV;
+                break;
+            case "+":
+                this.operation = ADD;
+                break;
+            case "-":
+                this.operation = SUB;
+                break;
+            default:
+                this.operation = -1;
+
+        }
+        return this.operation;
+    }
+
+    private double getNum(String value) {
+        double num;
+        try {
+            num = Double.parseDouble(value);
+        } catch (Exception e) {
+            num = processRomeNumbers(value);
+        }
+        return num;
+    }
+
+    /*
+    Convert roman numerals to arabic ones.
+     */
+    private double processRomeNumbers(String value) {
+        int result = 0;
+        int correspondingNum = 0;
+        int numberOnTheRight = 0;
+        for (int i = value.length() - 1; i >= 0; i--) {
+            if (String.valueOf(value.charAt(i)).isEmpty())
+                continue;
+            try {
+                correspondingNum = arabicNumerals[getIndex(value.charAt(i))];
+            } catch (IndexOutOfBoundsException e) {
+                throw new ArithmeticException("Please enter valid numbers, either arabic or roman numerals are valid.");
+            }
+
+            if (i < value.length() - 1) {
+                result = numberOnTheRight <= correspondingNum ? result + correspondingNum : result - correspondingNum;
+            } else {
+                result = correspondingNum;
+            }
+            numberOnTheRight = correspondingNum;
+
+
+        }
+
+        return result;
+    }
+
+
+    /*
+    Get the index of roman numeral value in arabic numerals list.
+     */
+    private int getIndex(char romanNumeral) {
+        for (int i = 0; i < romanNumerals.length; i++) {
+            if (String.valueOf(romanNumeral).equals(romanNumerals[i])) {
+                // System.out.println("index is " + i + "for "+romanNumeral + "is equal to " + romanNumerals[i]);
+                return i;
+            }
+
+        }
+        // exception case
+        return -1;
+    }
+
+    public double getResult() {
+        return result;
+    }
+
+    /*
+    Multiplication method
+     */
+    private double Multiply(double num1, double num2) {
+        return num1 * num2;
+    }
+
+    /*
+    Division method
+     */
+    private double Divide(double num1, double num2) {
+        if (num2 == 0) {
+            System.out.println("The divisor (number 2) cannot be equal to 0!");
+            throw new ArithmeticException();
+        }
+        return num1 / num2;
+    }
+
+    /*
+    Addition method
+     */
+    private double Addition(double num1, double num2) {
+        return num1 + num2;
+    }
+
+    /*
+    Subtraction method
+     */
+    private double Subtract(double num1, double num2) {
+        return num1 - num2;
+    }
+
+
+}
